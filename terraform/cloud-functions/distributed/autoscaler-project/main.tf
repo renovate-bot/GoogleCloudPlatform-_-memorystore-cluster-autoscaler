5a8df328e4b022b7e904996673b012f8af387e36
Niel Markwick <nielm@users.noreply.github.com>
2024-10-31 17:26:23 +0100

    fix(deps): update terraform module versions (#8)

--- terraform/cloud-functions/distributed/autoscaler-project/main.tf
+++ terraform/cloud-functions/distributed/autoscaler-project/main.tf
@@ -18,17 +18,12 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 6.1.0"
+      version = "6.9.0"
     }
   }
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
-provider "google-beta" {
   project = var.project_id
   region  = var.region
 }
